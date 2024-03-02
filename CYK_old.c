daae1c4e923180ed780ddec40a9a4f9550be2b79
prepcb <123950776+prepcb@users.noreply.github.com>
2024-03-02 22:31:08 +0000

    refine CYK so S can only be in start position

--- CYK_old.c
+++ CYK_old.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-#define n 6 //variables
-#define N 5 //string length
+#define n 6
+#define N 5
 
 int S = 0;
 int A = 1;
@@ -16,6 +16,7 @@ int CFG[n][n][n]={0};
 int V[N][N][n]={0};
 
 
+
 int main(){
 CFG[A][B][S] = 1;
 CFG[A][B][C] = 1;
@@ -27,41 +28,12 @@ CFG[a][a][C] = 1;
 CFG[b][b][B] = 1;  
 
 int s[]={b,a,a,b,a}; //trial string
-
-
-
-
-int i,j,k,m,p,q,r;
-
-
+int i,j,k,l,m,mm;
 for(i=0;i<N;i++)
     for(j=0;j<n;j++)
         if(CFG[s[i]][s[i]][j]==1)
             V[i][i][j] = 1;  
 char Var[4]={'S','A','B','C'};
-
-for(k=0;k<N;k++)
-    for(i=0;i<n-k-1;i++){
-        j=i+1+k;
-        for(m=i;m<j;m++)
-            for(p=0;p<N;p++)
-                for(q=0;q<N;q++)
-                    if(V[i][m][p]==1&&V[m+1][j][q]==1)
-                        for(r=0;r<N;r++)
-                            if(CFG[p][q][r]==1){
-                                V[i][j][r]=1;
-
-                                //start: next bit only lets S be in the final V(0,N-1) position
-                                if(i==0&j==N-1){
-                                }
-                                else if(r==S)
-                                    V[i][j][r]=0;
-                                // end:
-                            }
-    }
-
-
-/*
 for(mm=1;mm<N;mm++){
 printf("\n");
     for(i=0;i<N-1;i++)
@@ -72,13 +44,13 @@ printf("\n");
                         if(V[i][i+m][j]==1&&V[i+m+1][i+mm][k]==1)
                             if(CFG[j][k][l]==1)
                                 V[i][i+mm][l] = 1;
-}*/
-for(k=0;k<N;k++){
+}
+for(mm=0;mm<N;mm++){
     printf("elements \n");
-    for(i=0;i<N-k;i++){
-        printf("(%d %d) {",i,i+k);
+    for(i=0;i<N-mm;i++){
+        printf("(%d %d) {",i,i+mm);
             for(j=0;j<n;j++)
-                if(V[i][i+k][j]==1)
+                if(V[i][i+mm][j]==1)
                     printf(" %c",Var[j]);
     printf("}\n");
     }
