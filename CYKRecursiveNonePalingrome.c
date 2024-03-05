be4fb26766c6ce6c092d7179e71f37dbd5bb650d
prepcb <123950776+prepcb@users.noreply.github.com>
2024-03-05 18:50:09 +0000

    finally got the CNForm for non palindrome

--- CYKRecursiveNonePalingrome.c
+++ CYKRecursiveNonePalingrome.c
@@ -10,28 +10,31 @@ The algorithm only finds the first viable path - better than the psuedo code!
 
 
 #include<stdio.h>
-#define n 11 //variables
-#define N 10 //string length
+#define n 10 //variables
+#define N 9 //string length
 
 //Variables are given integers
 #define  R  0
-#define  A  1
-#define  B  2
-#define  C  3
-#define  Q  4
-#define  T  5
-#define  X  6
-#define  Y  7
-#define  Z  8
-#define  a  9
-#define  b  10
+#define  Q  1
+#define  Y  2
+#define  Z  3
+#define  T  4
+#define  X  5
+#define  A  6
+#define  B  7
+#define  a  8
+#define  b  9
 
-char Var[] = {'R','A','B','C','Q','T','X','Y','Z'}; // for printing purposes
+char Var[] = {'R','Q','Y','Z','T','X','A','B','a','b'}; // for printing purposes
 int counter = 0;
 //CFG contains the grammar
 int CFG[n][n][n]={0};
 
-int s[]={b,a,b,a,a,b,a,a,a,b}; //trial string
+int W[N][N][n]={0}; //This table array is used to avoid repeated function calls lines 54 and 55.
+
+
+int s[]={b,a,b,b,a,b,b,a,b}; //trial string
+
 
 
 // V contains the chart of the resulting grammar
@@ -39,23 +42,23 @@ int s[]={b,a,b,a,a,b,a,a,a,b}; //trial string
 int V(int i,int j,int k){
     counter++;
     int m,p,q;
-    if(i==j)
-        if(CFG[s[i]][s[i]][k]==1){
-            printf("V(%d,%d) = %c\n",i,i,Var[k]);
-            return(1);
-        }
-        else
-            return(0);
+    if(i==j){
+        W[i][j][k]=CFG[s[i]][s[i]][k];
+        return(W[i][j][k]);
+    }
     else
             for(p=0;p<n-2;p++)
                 for(q=0;q<n-2;q++)
                     if(CFG[p][q][k]==1) //check all places in the grammar with an entry
-                        for(m=i;m<j;m++)
-                            if(V(i,m,p)==1&&V(m+1,j,q)==1)
-                                {
-                                    printf("V(%d,%d) = %c\n",i,j,Var[k]);
-                                    return(1);
-                                }
+                        for(m=i;m<j;m++){
+                            if(W[i][m][p]<0)W[i][m][p]=V(i,m,p);
+                            if(W[m+1][j][q]<0)W[m+1][j][q]=V(m+1,j,q);
+                            if(W[i][m][p]==1&&W[m+1][j][q]==1){
+                                W[i][j][k]=1;
+                                return(1);
+                            }
+                        }
+    W[i][j][k]=0;
     return(0);
 }
 
@@ -68,14 +71,15 @@ int main(){
 // Here we set up the Context Free Grammar productions
 // this cannot be done without Chomsky normal form!
 
-CFG[A][Q][R] = 1;   // S -> AB
-CFG[T][B][Q] = 1;   // C -> AB
-CFG[B][C][R] = 1;   // S -> BC
-CFG[T][A][C] = 1;   // B -> CC
-CFG[X][Y][R] = 1;   // A -> BA
-CFG[R][X][Y] = 1;   // A -> a   in fact we only use the first slot; see line 43
-CFG[X][Z][T] = 1;   // C -> a                   ""
-CFG[T][X][Z] = 1;   // B -> b                   ""
+CFG[X][Q][R] = 1;  
+CFG[A][Y][R] = 1;  
+CFG[B][Z][R] = 1;  
+CFG[A][B][R] = 1;  
+CFG[B][A][R] = 1;  
+CFG[R][X][Q] = 1;  
+CFG[T][B][Y] = 1;
+CFG[T][A][Z] = 1; 
+CFG[X][T][T] = 1;
 CFG[a][a][T] = 1;
 CFG[b][b][T] = 1;
 CFG[a][a][X] = 1;
@@ -86,6 +90,11 @@ CFG[b][b][B] = 1;
 
 
 int i,j,k,l,m;
+
+for(i=0;i<N;i++)
+    for(j=0;j<N;j++)
+        for(k=0;k<n;k++)
+            W[i][j][k]=-1;
 
 
 /*
@@ -98,14 +107,8 @@ int i,j,k,l,m;
                 printf("%c ",Var[j]);
         printf("\n");
     }
-  */ 
+ */ 
 
-k=1;
-for(i=0;i<10;i++)
-    for(j=0;j<N;j++){
-        s[j] = i&k;
-        k=2*k;
-}
 
 if(V(0,N-1,0)==1)
     printf("accept\n");
--- CYKRecursiveNonePalingrome3.c
+++ CYKRecursiveNonePalingrome3.c
@@ -10,7 +10,6 @@ The algorithm only finds the first viable path - better than the psuedo code!
 
 
 #include<stdio.h>
-
 #define n 11 //variables
 #define N 10 //string length
 
@@ -28,56 +27,54 @@ The algorithm only finds the first viable path - better than the psuedo code!
 #define  b  10
 
 char Var[] = {'R','A','B','C','Q','T','X','Y','Z'}; // for printing purposes
-
-
 int counter = 0;
 //CFG contains the grammar
 int CFG[n][n][n]={0};
-
-int s[]={b,a,a,a,b,b,a,a,a,b}; //trial string
 int W[N][N][n]={0};
+
+int s[]={b,a,a,a,a,b,a,a,a,b}; //trial string
+
 
 // V contains the chart of the resulting grammar
 
 int V(int i,int j,int k){
     counter++;
+   // printf("%d %d %d\n",i,j,k);
     int m,p,q;
     if(i==j)
-        if(CFG[s[i]][s[i]][k]==1){
-            printf("V(%d,%d) = %c\n",i,i,Var[k]);
-            return(1);
-        }
-        else
-            return(0);
+        return(CFG[s[i]][s[i]][k]);
     else
             for(p=0;p<n-2;p++)
                 for(q=0;q<n-2;q++)
                     if(CFG[p][q][k]==1) //check all places in the grammar with an entry
                         for(m=i;m<j;m++){
-                            if(V(i,m,p)==1&&V(m+1,j,q)==1)
-                                    {
-                                        printf("V(%d,%d) = %c\n",i,j,Var[k]);
-                                       // W[i][j][k]=1;
-                                        return(1);
+                          //  if(W[i][m][p]<0)
+                                W[i][m][p]=V(i,m,p);
+                           // if(W[m+1][j][q]<0)
+                                W[m+1][j][q]=V(m+1,j,q);
+                            if(W[i][m][p]==1&&W[m+1][j][q]==1)
+                                return(1);
                                 }
-                        }
     return(0);
 }
 
 
 int main(){
 
+
+
+
 // Here we set up the Context Free Grammar productions
 // this cannot be done without Chomsky normal form!
 
-CFG[A][Q][R] = 1;   //R -> AQ etc
-CFG[T][B][Q] = 1;  
-CFG[B][C][R] = 1;  
-CFG[T][A][C] = 1;  
-CFG[X][Y][R] = 1;  
-CFG[R][X][Y] = 1;  
-CFG[X][Z][T] = 1;   
-CFG[T][X][Z] = 1; 
+CFG[A][Q][R] = 1;   // S -> AB
+CFG[T][B][Q] = 1;   // C -> AB
+CFG[B][C][R] = 1;   // S -> BC
+CFG[T][A][C] = 1;   // B -> CC
+CFG[X][Y][R] = 1;   // A -> BA
+CFG[R][X][Y] = 1;   // A -> a   in fact we only use the first slot; see line 43
+CFG[X][Z][T] = 1;   // C -> a                   ""
+CFG[T][X][Z] = 1;   // B -> b                   ""
 CFG[a][a][T] = 1;
 CFG[b][b][T] = 1;
 CFG[a][a][X] = 1;
@@ -88,27 +85,15 @@ CFG[b][b][B] = 1;
 
 
 int i,j,k,l,m;
-
-//initialise table: used to avoid too many function calls
 for(i=0;i<N;i++)
     for(j=0;j<N;j++)
         for(k=0;k<n;k++)
             W[i][j][k]=-1;
-/*
-// This reproduces the whole table
-    for(k=0;k<N;k++)
-    for(i=0;i<N-k;i++){
-        printf("(%d,%d)",i,i+k);
-        for(j=0;j<n-2;j++)
-            if(V(i,i+k,j)==1)
-                printf("%c ",Var[j]);
-        printf("\n");
-    }
-  */ 
 
 
 
-if(V(0,N-2,0)==1)
+
+if(V(0,N-1,0)==1)
     printf("accept\n");
 else    
     printf("reject\n");
